029b8364f09ca6630bbab45963ed0035ea364d32
tushar patel <pateltushar4242@gmail.com>
2022-08-16 22:25:27 -0400

    while loop and for...in & of

--- BootCamp/test.js
+++ BootCamp/test.js
@@ -84,13 +84,86 @@ for(let i=0; i<frds.length; i++){
 
 
 
+const SECRET = "BabyHippo";
+
+let guess = prompt("enter the code")
+
+while(guess !== SECRET){
+    guess = prompt("enter code");
+}
+console.log("Congrats!!")
+
+
+// -----------------------
+let input = prompt("enter something")
+while(true){
+    input = prompt(input);
+    if(input === "stop copying"){
+        break;
+    }
+}
+ console.log("win")       
+
+//////------------------------
+let maximun = parseInt(prompt("enter number"));
+while(!maximun) {
+    maximun = parseInt(prompt("enter valid num"))
+}
+
+const targetNum = Math.floor(Math.random() * maximun) + 1 
+console.log(targetNum)
+
+let guess2 = prompt("enter your first num");
+let attempts = 1;
+while(parseInt(guess2) !== targetNum){
+    if(guess2 === 'q')
+    break;
+    attempts++;
+    if(guess2 > targetNum){
+        guess2 = prompt("Tooo high, enter new")
+    }
+    else{
+        guess2 = prompt("Too low, enter new")
+    }
+}
+console.log(`You Got It, you took ${attempts} attempts `)
 
 
 
+// -------------- for... of loop
+for (let fr of frds){
 
+    for(let j of fr){
+        console.log(j)
+    }
+}
 
+for(let char of "hello world"){
+    console.log(char)
+} 
 
+// --- objects with for...of
 
+const testScore = {
+    tp : 70,
+    tushar:80,
+    tarun:10,
+    rohan:100,
+    mitesh:99,
+    mayank:98
+} 
+for (let test in testScore){
+    console.log(test)
+}
+for (let test in testScore){
+    console.log(`${test} scored ${testScore[test]}`)
+}
+
+let total = 0;
+for(let score of Object.values(testScore)){
+    total += score;
+    console.log(total);
+}
 
 
 
